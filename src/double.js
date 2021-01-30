a4c1e7cae64b0ee079571f1e0e7e82a545ecbd8e
Yusuke Sato <yusuke.sato@ys-office.me>
2021-01-30 23:06:08 +0900

    add source

---
+++ src/double.js
@@ -0,0 +1,5 @@
+function double(x) {
+    return x * 4;
+}
+
+module.exports = double;
---
+++ src/double.test.js
@@ -0,0 +1,5 @@
+const double = require('./double')
+
+test('double 2 to equal 8', () => {
+    expect(double(2)).toBe(8);
+});
